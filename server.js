1c66b745bbbaf49872b66187b3f0078f503feea7
Dmitry Ivanov <dmitry.ivanov.od@gmail.com>
2015-06-11 15:11:53 +0300

    Improved organization and structure of the application

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // set up ------------------------
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var express        =  require('express');
+var app            =  express();
+var mongoose       =  require('mongoose');
+var port           =  process.env.PORT || 8080;
+var database       =  require('./config/database');
+var morgan         =  require('morgan');
+var bodyParser     =  require('body-parser');
+var methodOverride =  require('method-override');
 
 // configuration -----------------
-mongoose.connect('mongodb://user:pass@ds043952.mongolab.com:43952/firstdb');
+mongoose.connect(database.url);
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
@@ -16,62 +18,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
-// define model ------------------
-var Todo = mongoose.model('Todo', {
-	text : String
-});
-
 // routes ------------------------
-
-	// api -----------------------
-	// get all todos
-	app.get('/api/todos', function(req, res) {
-		Todo.find(function(err, todos) {
-			if(err)
-				res.send(err)
-
-			res.json(todos);
-		});
-	});
-
-	// create todo and send back all todos after creation
-	app.post('/api/todos', function(req, res) {
-		Todo.create({
-			text : req.body.text,
-			done : false
-		}, function(err, todo) {
-			if (err)
-				res.send(err);
-
-			Todo.find(function(err, todos) {
-				if (err)
-					res.send(err)
-				res.json(todos);
-			});
-		});
-	});
-
-	// delete a todo
-	app.delete('/api/todos/:todo_id', function(req, res) {
-		Todo.remove({
-			_id : req.params.todo_id
-		}, function(err, todo) {
-			if (err)
-				res.send(err);
-
-			Todo.find(function(err, todos) {
-				if (err)
-					res.send(err)
-				res.json(todos);
-			});
-		});
-	});
-
-// application ----------------
-app.get('*', function(req, res) {
-	res.sendfile('./public/index.html');
-});
+require('./app/routes')(app);
 
 // listen ------------------------
-app.listen(8080);
-console.log("App listening on port 8080");
+app.listen(port);
+console.log("App listening on port "+ port);
